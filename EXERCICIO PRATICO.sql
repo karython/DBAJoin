f0ef0c541bc0c31490e6010062081c93b245004f
Vih <vitoriasilva415n@gmail.com>
2023-10-20 11:34:13 -0300

    Select groupy be

--- EXERCICIO PRATICO.sql
+++ EXERCICIO PRATICO.sql
@@ -1,4 +1,5 @@
 -- Criar a base de dados
+drop database Gerenciador;
 CREATE DATABASE Gerenciador;
 
 -- Usar a base de dados
@@ -10,7 +11,223 @@ CREATE TABLE Clientes (
     Nome VARCHAR(50) NOT NULL,
     Email VARCHAR(100),
     Telefone VARCHAR(15)
+    
 );
+desc clientes;
+select * from clientes;
+
+/*1*/
+create table Tipo_de_Cliente
+(
+Id_tipo_Cliente int auto_increment primary key,
+Tipo_de_pessoa varchar(20)
+);
+
+/*2*/
+alter table Clientes add  tipo_cliente int;
+
+
+/*3*/
+insert into Tipo_de_Cliente(Tipo_de_Pessoa)values
+('Juridica'),('Física');
+
+
+alter table clientes add constraint fk_tipo_cliente foreign key (tipo_cliente) references Tipo_de_Cliente(id_tipo_cliente) on delete cascade ;
+truncate table clientes;
+/*4*/
+select ClienteID ,Nome
+from Clientes
+where nome
+like '%a';
+
+update Clientes set tipo_cliente= null where ClienteID >1;
+update Clientes set tipo_cliente= 2 where ClienteID =1;
+update Clientes set tipo_cliente= 2 where ClienteID >=4 and ClienteID <=6;
+update Clientes set tipo_cliente= 2 where ClienteID =8;
+update Clientes set tipo_cliente= 2 where ClienteID >=11 and ClienteID <=14;
+update Clientes set tipo_cliente= 2 where ClienteID =16;
+update Clientes set tipo_cliente= 2 where ClienteID =18;
+update Clientes set tipo_cliente= 2 where ClienteID >=21 and ClienteID <=22;
+update Clientes set tipo_cliente= 2 where ClienteID >=24 and ClienteID <=26;
+update Clientes set tipo_cliente= 2 where ClienteID >=29 and ClienteID <=31;
+update Clientes set tipo_cliente= 2 where ClienteID =33;
+update Clientes set tipo_cliente= 2 where ClienteID =36 and ClienteID <=36;
+update Clientes set tipo_cliente= 2 where ClienteID =39;
+update Clientes set tipo_cliente= 2 where ClienteID =40;
+update Clientes set tipo_cliente= 2 where ClienteID =42;
+update Clientes set tipo_cliente= 2 where ClienteID =44;
+update Clientes set tipo_cliente= 2 where ClienteID >=47  AND ClienteID <=48;
+update Clientes set tipo_cliente= 2 where ClienteID >=50  AND ClienteID <=52 ;
+update Clientes set tipo_cliente= 2 where ClienteID >=56 AND ClienteID <=57;
+update Clientes set tipo_cliente= 2 where ClienteID =59;
+update Clientes set tipo_cliente= 2 where ClienteID >=62 AND ClienteID <=64;
+update Clientes set tipo_cliente= 2 where ClienteID >=66 AND ClienteID <=67;
+update Clientes set tipo_cliente= 2 where ClienteID >=71 and ClienteID <=74;
+update Clientes set tipo_cliente= 2 where ClienteID =76;
+update Clientes set tipo_cliente= 2 where ClienteID >=79 AND ClienteID <=82;
+update Clientes set tipo_cliente= 2 where ClienteID =84;
+update Clientes set tipo_cliente= 2 where ClienteID =86;
+update Clientes set tipo_cliente= 2 where ClienteID =88;
+update Clientes set tipo_cliente= 2 where ClienteID =90;
+
+
+select ClienteID ,Nome
+from Clientes
+where nome
+like  '%o';
+
+update Clientes set tipo_cliente=1 where ClienteID=15;
+update Clientes set tipo_cliente=1 where ClienteID=27;
+update Clientes set tipo_cliente=1 where ClienteID=37;
+update Clientes set tipo_cliente=1 where ClienteID=45;
+update Clientes set tipo_cliente=1 where ClienteID=54;
+update Clientes set tipo_cliente=1 where ClienteID=61;
+update Clientes set tipo_cliente=1 where ClienteID=69;
+update Clientes set tipo_cliente=1 where ClienteID=77;
+update Clientes set tipo_cliente=1 where ClienteID=85;
+update Clientes set tipo_cliente=1 where ClienteID=89;
+
+/*5*/
+Select * from clientes;
+
+/*6*/
+SELECT nome, tipo_cliente
+from clientes;
+
+
+/*7*/
+SELECT nome, tipo_cliente
+from clientes
+where tipo_cliente is null;
+
+
+/*8*/
+select ClienteID, Nome, Tipo_de_pessoa
+From Clientes
+left join Tipo_de_Cliente 
+on Clientes.tipo_cliente = Tipo_de_cliente.Id_tipo_Cliente ;
+
+
+
+/*9*/
+select* from pedidos;
+insert into pedidos(ClienteID, DataPedido, ValorTotal) values
+(5,'2023-10-17', 46.50),/*produto2 3*/
+(5,'2023-10-17', 20.75),/*produto3 1*/
+(5,'2023-10-17', 12.25) ,/*produto5 1*/
+
+
+(25,'2023-10-17',5.50 ), /*produto6 1*/
+(25,'2023-10-17',30.99), /*produto7 1*/
+(25,'2023-10-17',8.75 ), /*produto8 1*/
+
+
+(20,'2023-10-17',37.00 ),/*produto9 2*/
+(20,'2023-10-17',11.00 ),/*produto6  2*/
+(20,'2023-10-17',36.75 ),/*produto5 3 */
+
+
+(9,'2023-09-12',47.94 ),/*produto4 6*/
+(9,'2023-09-12',15.50 ),/*produto2  1 */
+(9,'2023-09-12',25.99 ),/*produto10 1 */
+(9,'2023-09-12',16.50 ),/*produto6 3*/
+(9,'2023-09-12',43.96 ),/*produto1 4 */
+(9,'2023-09-12',18.50 ),/*produto9 1 */
+
+
+(43,'2023-09-12',52.50),/*produto8 6*/
+(43,'2023-09-12',20.75),/*produto3 1*/
+(43,'2023-09-12',30.99),/*produto7 1*/
+(43,'2023-09-12',7.99),/*produto4 1*/
+(43,'2023-09-12',15.50),/*produto2 1*/
+(43,'2023-09-12',12.25),/*produto5 1*/
+
+(10,'2023-10-15',32.97),/*1* 3*/
+(10,'2023-10-15',77.50),/*2* 5*/
+(10,'2023-10-15',41.50),/*3* 2*/
+(10,'2023-10-15',31.96),/*4* 4 */
+(10,'2023-10-15',61.25),/*5* 5*/
+(10,'2023-10-15',33.00),/*6* 6*/
+(10,'2023-10-15',61.98),/*7* 2*/
+(10,'2023-10-15',26.25),/*8* 3*/
+(10,'2023-10-15',92.50),/*9* 5*/
+(10,'2023-10-15',25.99),/*10* 1*/
+
+(40,'2023-10-15',10.99),/*1* 1*/
+(40,'2023-10-15',62.00),/*2* 4*/
+(40,'2023-10-15',20.75),/*3* 1*/
+(40,'2023-10-15',7.99),/*4* 1*/
+(40,'2023-10-15',12.25),/*5* 1*/
+(40,'2023-10-15',5.50),/*6* 1*/
+(40,'2023-10-15',30.99),/*7* 1*/
+(40,'2023-10-15',8.75),/*8* 1*/
+(40,'2023-10-15',18.50),/*9* 1*/
+(40,'2023-10-15',25.99),/*10* 1*/
+
+
+(89,'2023-10-15',10.99),/*1* 1*/
+(89,'2023-10-15',15.50),/*2* 1*/
+(89,'2023-10-15',20.75),/*3* 1*/
+(89,'2023-10-15',7.99),/*4* 1*/
+(89,'2023-10-15',12.25),/*5* 1*/
+(89,'2023-10-15',5.50),/*6* 1*/
+(89,'2023-10-15',30.99),/*7* 1*/
+(89,'2023-10-15',8.75),/*8* 1*/
+(89,'2023-10-15',18.50),/*9* 1*/
+(89,'2023-10-15',25.99);/*10* 1*/
+
+
+select * from produtos;
+
+/*10*/
+
+select * 
+from pedidos
+where DataPedido
+between '2023-10-01' and '2023-10-31';
+
+/*11*/
+
+select sum(Valortotal) as Valordetodospedidos
+from Pedidos
+;
+
+
+/*12*/
+select Nome ,DataPedido
+from Pedidos
+join clientes on clientes.clienteID = pedidos.PedidoID
+where DataPedido
+between '2023-10-15'and '2023-10-15'
+order by Nome ASC;
+;
+
+
+/*13*/
+
+select Nome, sum(Valortotal) as ValorTotal
+from clientes
+join pedidos
+on clientes.clienteID = pedidos.ClienteID
+Group by Nome;
+
+
+/*14*/
+select Produtos.ProdutoID,NomeProduto,(Quantidade)
+from produtos
+left join itenspedido on Produtos.ProdutoID = Itenspedido.ProdutoID 
+group by quantidade 
+having quantidade >1;
+
+
+/*15*/
+select Nome,sum(Quantidade)
+from itenspedido
+left join clientes on clientes.ClienteID = Itenspedido.ProdutoID 
+group by quantidade asc;
+
+
+
 
 -- Criar a tabela 'Pedidos'
 CREATE TABLE Pedidos (
@@ -39,11 +256,17 @@ CREATE TABLE ItensPedido (
 );
 
 
+
+
+
+
+select* from Tipo_de_Cliente;
 select * from clientes;
 select * from produtos;
 select * from pedidos;
 select * from ItensPedido;
 
+delete from clientes where ClienteID >90;
 
 INSERT INTO Clientes (Nome, Email, Telefone) VALUES
     ('João Silva', 'joao@email.com', '(11) 1234-5678'),
@@ -201,157 +424,6 @@ INSERT INTO Pedidos (ClienteID, DataPedido, ValorTotal) VALUES
     (20, '2023-10-29', 110.25);
     
     
- /*1*/
- Select * from Clientes
- order by nome Asc;
- 
- 
- 
- 
-/*2*/
-SELECT * FROM Produtos
-where preco
-between 10 and 31
-order by preco Desc
-limit 5;
-
-
- 
- /*3*/
-
-select *from Pedidos
-where ValorTotal
-between 120 and 190
-order by ValorTotal asc
-limit 1;
-
-
-/*4*/
-select Clientes.Nome, Pedidos.Valortotal
-from Clientes
-join Pedidos
-on Pedidos.ClienteID = Clientes.ClienteID;
-
-/*5*/
-select Pedidos.PedidoID,Clientes.ClienteID ,Clientes.Nome, Pedidos.Valortotal, Pedidos.DataPedido
-from Clientes
-join Pedidos on Pedidos.ClienteID = Clientes.ClienteID;
-
-
-
-/*6*/
-select Clientes.Nome, Pedidos.Valortotal
-from Clientes
-join Pedidos
-on Pedidos.ClienteID = Clientes.ClienteID
-where Valortotal
-between 200 and 270;
-
-
-
-/*7*/
-select Clientes.Nome, Pedidos.Valortotal
-from Clientes
-join Pedidos
-on Pedidos.ClienteID = Clientes.ClienteID
-where Valortotal
-between 200 and 270
-order by Clientes.Nome ASC;
-
-
-/*8*/
-select * From Pedidos where DataPedido
-between'2023-09-27'and'2023-10-08'
-order by DataPedido Asc
-;
-
-/*9*/
-
-SELECT * FROM Produtos
-where preco
-between 10 and 31
-order by preco ASC
-limit 5;
-
-
-/*10*/
-
-
-select Clientes.ClienteID ,Clientes.nome, Produtos.NomeProduto,ItensPedido.Quantidade
-from itensPedido
-join produtos on   itensPedido.PedidoId =  Produtos.ProdutoID 
-join Clientes on itensPedido.PedidoID = Clientes.clienteID
-where PedidoID= 3
-;
-
-
-
-/*11*/
-
-select nome
- from Clientes
-join Pedidos on Clientes.clienteID = Pedidos.PedidoID 
-where nome
-like '%o';
-
-/*12*/
-
-select Clientes.nome, Produtos.NomeProduto
-from Clientes 
-JOIN Pedidos on  Clientes.ClienteID= Pedidos.PedidoID
-JOIN ItensPedido on Pedidos.PedidoID = ItensPedido.PedidoID
-join Produtos on  Produtos.ProdutoID = ItensPedido.ProdutoID;
-
-
-
-/*13*/
-select Clientes.nome, Clientes.ClienteID, Pedidos.ValorTotal
-from clientes 
-left join pedidos on Pedidos.PedidoID = Clientes.ClienteID
-where ValorTotal is null;
-
-
-/*14*/
-
-select nome
-from clientes
-where nome
-like 'a%';
-
-
-
-/*15*/
-Select Clientes.ClienteID, Clientes.nome ,Pedidos.PedidoID, Pedidos.DataPedido, Produtos.NomeProduto,Produtos.preco
-From Pedidos
-join Produtos on Pedidos.PedidoID = Produtos.ProdutoID
-join Clientes on Pedidos.PedidoID = Clientes.ClienteID
-where DataPedido
-between '2023-09-01' and '2023-10-29'
-limit 7;
-
-/*16*/
-
-select *from clientes
-where nome
-like '%arc%';
-
-/*17*/
-
-select Pedidos.PedidoID,Produtos.NomeProduto, Pedidos.DataPedido,Pedidos.ValorTotal,ItensPedido.Quantidade
-from ItensPedido
-join produtos on Produtos.ProdutoID = ItensPedido.ItemID
-join Pedidos on Pedidos.PedidoID = ItensPedido.ItemID;
-
-
-
-/*18*/
-select Clientes.ClienteID, Clientes.Nome ,Pedidos.PedidoID, Pedidos.DataPedido,Pedidos.ValorTotal,Produtos.NomeProduto,ItensPedido.Quantidade
-from Pedidos
-join produtos on  Pedidos.PedidoID = Produtos.ProdutoID
-join Clientes on Pedidos.PedidoID = Clientes.ClienteID
-join ItensPedido on Pedidos.PedidoID = ItensPedido.ItemID
-order by ValorTotal desc
-limit 5;
 
 
 
@@ -377,3 +449,62 @@ INSERT INTO ItensPedido (PedidoID, ProdutoID, Quantidade) VALUES
     (9, 6, 2),
     (10, 8, 1),
     (10, 10, 2);
+    
+    
+INSERT INTO ItensPedido (PedidoID, ProdutoID, Quantidade) VALUES
+    (44, 2, 3),
+    (45, 3, 1),
+    (46, 5, 1),
+
+    (47, 6, 1),
+    (48, 7, 1),
+    (49, 8, 1),
+
+    (50, 9, 2),
+    (51, 6, 2),
+    (52, 5, 3),
+
+    (53, 4, 6),
+    (54, 2, 1),
+    (55, 10, 1),
+ (56, 6, 3),
+    (57, 1, 4),
+    (58, 9, 1),
+    (59, 8, 6),
+
+    (60,3, 1),
+    (61, 7, 1),
+    (62, 4, 1),
+    (63, 2, 1),
+    (64, 5, 1),
+    (65, 1, 3),
+    (66, 2, 5),
+    (67, 3, 2),
+    (68, 4, 4),
+    (69, 5, 5),
+    (70, 6, 6),
+    (71, 7, 2),
+    (72, 8, 3),
+    (73, 9, 5),
+    (74, 10,1),
+
+    (75, 1, 1),
+    (76, 2, 4),
+    (77, 3, 1),
+    (78, 4, 1),
+    (79, 5, 1),
+    (80, 6, 1),
+    (81, 7, 1),
+    (82, 8, 1),
+    (83, 9, 1),
+    (84, 10, 1),
+
+    (86, 1, 1),
+    (87, 2, 1),
+    (88, 3, 1),
+    (89, 4, 1),
+    (90, 5, 1),
+    (91, 6, 1),
+    (92, 7, 1),
+    (93, 8, 1),
+    (94, 9, 1);
